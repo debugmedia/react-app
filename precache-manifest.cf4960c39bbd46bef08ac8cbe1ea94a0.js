d2c8b1c8b32a6b30e02ff4eaa1c5b2586cb8c85b
debugmedia <debugmedia@gmail.com>
2020-05-04 00:40:24 +0400

    Updates

---
+++ precache-manifest.cf4960c39bbd46bef08ac8cbe1ea94a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f4db53214b6c643374902edfc756d0",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "ca8e5955725ed02f2e10",
+    "url": "/react-app/static/css/main.bd595ee1.chunk.css"
+  },
+  {
+    "revision": "9662ebf8fdaa54da8318",
+    "url": "/react-app/static/js/2.9a5a9f82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.9a5a9f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8e5955725ed02f2e10",
+    "url": "/react-app/static/js/main.6d6f51b5.chunk.js"
+  },
+  {
+    "revision": "2859edfa3cf6df56101c",
+    "url": "/react-app/static/js/runtime-main.b41e6653.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
